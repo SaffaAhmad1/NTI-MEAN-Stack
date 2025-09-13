8f82bd8fc3af89f1770638f03a9c21c7b0478f70
Saffa Ahmed <84234873+SaffaAhmad1@users.noreply.github.com>
2025-09-13 11:21:32 +0300

    Add files via upload

---
+++ JavaSceipt/Day7/main.js
@@ -0,0 +1,10 @@
+var fun =(a,b) => a+b
+
+console.log(fun(5,10));
+
+let firstName = "Saffa";
+let lastName= "Ahmed";
+let age =25;
+
+let fulllName = `${firstName} ${lastName} , Age: ${age}`
+console.log(fulllName);
