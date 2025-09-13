ffebf93d57a4e9e4566f04ab400fdf524094b13a
Saffa Ahmed <84234873+SaffaAhmad1@users.noreply.github.com>
2025-09-13 11:20:17 +0300

    Add files via upload

---
+++ JavaSceipt/Day6/main.js
@@ -0,0 +1,23 @@
+var age = window.prompt("Enter Your Age");
+// if (age <= 18) {
+//   console.log("young");
+// } else if (age >= 18 && age < 60) {
+//   console.log("adult");
+// } else if (age >= 60) {
+//   console.log("sdfghjkl");
+// } else {
+//   console.log("noooooo");
+// }
+
+age <= 18
+  ? console.log("young")
+  : age >= 18 && age < 60
+  ? console.log("adult")
+  : age >= 60
+  ? console.log("sdfghjkl")
+  : console.log("noooooo");
+
+  var x;
+  [1,2,3].forEach(function(elem,index,arr){
+    
+  })
